dc8f796e0f41a8a46746375c882c3d5a8cc6e055
Md A Habib <mhabib19@gmail.com>
2025-08-31 19:34:22 -0500

    Frontend setup - react app with typescript, tailwind css, nginx. Also Frontend Dcokerfile and complete docker compose file. Basic frontend app now running.

---
+++ card-processor/frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Card Processor</h1>
+        <p>Frontend setup successful!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
